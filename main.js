c5eb01c0a93a98735b7438df33fb4e9fbf2ddf22
Kevin Choy <kevinchoy22@gmail.com>
2019-03-07 22:46:58 -0800

    current weather and location render to index html

--- main.js
+++ main.js
@@ -4,24 +4,31 @@ $(document).ready(function() {
   
   function getLocationKey(zip) {
     let link = "http://dataservice.accuweather.com/locations/v1/postalcodes/US"
-    + "/search?apikey=" + secretKey + "&q=" + zip + "&language=en-us&details=false";
+      + "/search?apikey=" + secretKey + "&q=" 
+      + zip + "&language=en-us&details=false";
+    console.log(link);
 
     $.getJSON(link)
       .done(function(data) {
         let locationKey = data[0].Key;
+        let state = data[0].AdministrativeArea.EnglishName;
+        let city = data[0].EnglishName;
+        let html = city + ", " + state;
+
+        $("#cityName").html(html);
+
         getForcastLocation(locationKey);
       })
       .fail(function( textStatus, error ) {
         var err = textStatus + ", " + error;
         console.log( "Request Failed: " + err );
-    
-    });
+      });
   }
 
 
   function getForcastLocation(zip) {
-    let link = "http://dataservice.accuweather.com/forecasts/v1/daily/1day/" 
-      + zip + "?apikey=" + secretKey + "&language=en-us&details=false&metric=false";
+    let link = "http://dataservice.accuweather.com/currentconditions/v1/" 
+      + zip + "?apikey=" + secretKey + "&language=en-us&details=false";
 
    
     console.log(link);
@@ -29,12 +36,15 @@ $(document).ready(function() {
     $.getJSON(link)
       .done(function(data) {
       // console.log(data);
+         
         var conditions = data[0];
         var temp = conditions.Temperature.Imperial;
         let html = conditions.WeatherText + ", " + temp.Value + " " + temp.Unit;
+        $("#currentConditions").html(html);
       })
       .fail(function(textStatus, error) {
         let html = "No info at this time";
+        $("#currentConditions").html(html);
       });
 
   }
