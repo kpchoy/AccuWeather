73d1664f289ce4fc0c8a9cfaa4f34cec1b6a8689
Kevin Choy <kevinchoy22@gmail.com>
2019-03-11 12:36:34 -0700

    data for all three days no styling yet

--- main.js
+++ main.js
@@ -10,83 +10,71 @@ $(document).ready(function() {
 
     $.getJSON(link)
       .done(function(data) {
-
+        // debugger; 
         if (typeof data[0] === "undefined") {
           $("#main").html("");
         }
 
         let locationKey = data[0].Key;
-        let state = data[0].AdministrativeArea.EnglishName;
-        let city = data[0].EnglishName;
+        let state = data[0].AdministrativeArea.ID.toUpperCase();
+        let city = data[0].EnglishName.toUpperCase();
         let html = city + ", " + state;
 
         $("#city-name").html(html);
 
-        getForcastLocation(locationKey);
-        getTodaysForcast(locationKey);
+        get5DayForecast(locationKey);
       })
       .fail(function( ) {
         $("#main").html("");
       });
   }
 
-
-  function getForcastLocation(zip) {
-    let link = "http://dataservice.accuweather.com/currentconditions/v1/" 
-      + zip + "?apikey=" + secretKey + "&language=en-us&details=false";
-    // console.log("forcatlocation",link);
-
+  function get5DayForecast(zip) {
+    let link = "http://dataservice.accuweather.com/forecasts/v1/daily/5day/"
+    + zip + "?apikey=" + secretKey + "&language=en-us&details=false&metric=false";
+    console.log("5 Day forcast", link);
+    
     $.getJSON(link)
       .done(function(data) {
-        var conditions = data[0];
-        var temp = conditions.Temperature.Imperial.Value + " " 
-          + conditions.Temperature.Imperial.Unit;
-        let html = conditions.WeatherText;
-        $("#current-temp").html(temp);
-        $("#current-phrase").html(html);
+        let dayIconPhrase = data.DailyForecasts[0].Day.IconPhrase;
+        let dayIcon = iconLinkGen(data.DailyForecasts[0].Day.Icon);
+        let tempMin = data.DailyForecasts[0].Temperature.Minimum.Value;
+        let tempMax = data.DailyForecasts[0].Temperature.Maximum.Value;
+        $("#day-phrase").html(dayIconPhrase);
+        $("#day-img").attr("src", dayIcon);
+        $("#temp-min").html(tempMin);
+        $("#temp-max").html(tempMax);
 
-        let iconNum = data[0].WeatherIcon;
-        let icon = iconLinkGen(iconNum);
-        $("#weather-img").attr("src", icon);
+        let date2 = new Date(data.DailyForecasts[1].Date);
+        let date2Name = days[date2.getDay()];
+        let day2IconPhrase = data.DailyForecasts[1].Day.IconPhrase;
+        let day2Icon = iconLinkGen(data.DailyForecasts[1].Day.Icon);
+        let temp2Min = data.DailyForecasts[1].Temperature.Minimum.Value;
+        let temp2Max = data.DailyForecasts[1].Temperature.Maximum.Value;
+        $("#day2-day").html(date2Name);
+        $("#day2-phrase").html(day2IconPhrase);
+        $("#day2-img").attr("src", day2Icon);
+        $("#day2-temp-min").html(temp2Min);
+        $("#day2-temp-max").html(temp2Max);
+
+        let date3 = new Date(data.DailyForecasts[2].Date);
+        let date3Name = days[date3.getDay()];
+        let day3IconPhrase = data.DailyForecasts[2].Day.IconPhrase;
+        let day3Icon = iconLinkGen(data.DailyForecasts[2].Day.Icon);
+        let temp3Min = data.DailyForecasts[2].Temperature.Minimum.Value;
+        let temp3Max = data.DailyForecasts[2].Temperature.Maximum.Value;
+        $("#day3-day").html(date3Name);
+        $("#day3-phrase").html(day3IconPhrase);
+        $("#day3-img").attr("src", day3Icon);
+        $("#day3-temp-min").html(temp3Min);
+        $("#day3-temp-max").html(temp3Max);
+
         
       })
       .fail(function(textStatus, error) {
         $("#main").html("");
       });
 
-  }
-
-  function getTodaysForcast(zip) {
-    let link = "http://dataservice.accuweather.com/forecasts/v1/daily/1day/"
-      + zip + "?apikey=" + secretKey + "&language=en-us&details=false";
-    console.log("todays forcast", link);
-
-    $.getJSON(link)
-      .done(function(data) {
-        let date = data.DailyForecasts[0].Date.substring(0,10);
-        let dayIconPhrase = data.DailyForecasts[0].Day.IconPhrase;
-        let dayIcon = iconLinkGen(data.DailyForecasts[0].Day.Icon);
-
-        let nightIconPhrase = data.DailyForecasts[0].Night.IconPhrase;
-        let nightIcon = iconLinkGen(data.DailyForecasts[0].Night.Icon);
-
-        let tempMin = data.DailyForecasts[0].Temperature.Minimum.Value + " "
-          + data.DailyForecasts[0].Temperature.Minimum.Unit;
-        let tempMax = data.DailyForecasts[0].Temperature.Maximum.Value + " "
-          + data.DailyForecasts[0].Temperature.Maximum.Unit;
- 
-        $("#todays-date").html(date);
-        $("#day-phrase").html(dayIconPhrase);
-        $("#day-img").attr("src", dayIcon);
-        $("#night-phrase").html(nightIconPhrase);
-        $("#night-img").attr("src", nightIcon);
-        $("#temp-min").html(tempMin);
-        $("#temp-max").html(tempMax);
-
-      })
-      .fail(function(textStatus, error) {
-        $("#main").html("");
-      });
   }
 
   function getParameterByName(name, url) {
@@ -108,6 +96,8 @@ $(document).ready(function() {
   var zip = '94016';
   getLocationKey(zip);
 });
+
+var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
 
 function iconLinkGen(iconNum) {
   if (iconNum < 10) {
